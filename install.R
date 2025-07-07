65b60b8ae0d7a7889399035bcd953d99e1d32739
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2025-07-07 11:05:38 +0100

    Update install.R

--- install.R
+++ install.R
@@ -2,6 +2,7 @@ install.packages("IRkernel")
 install.packages("tidyverse")  
 install.packages("ggalluvial")
 install.packages("rnaturalearth")
+install.packages("units")
 install.packages("rnaturalearthdata")
 install.packages("sf")
 install.packages("stringr") # Add any other packages you need
